6c8c1f87a1d2e2a69c443926badd9c3a0bcc45f0
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-03-14 16:49:41 +0000

    one step at a time

--- cup.py
+++ cup.py
@@ -1,3 +1,11 @@
-def solve(self,a:int, b:int)->int:
-    pass
-print("Hello")
+def solve(a: int, b: int) -> None:
+    if a >= b:
+        print("Error: 'a' must be less than 'b'")
+    else:
+        for i in range(a, b):
+            print(i)
+
+a = 100
+b = 1000
+
+solve(a, b)
