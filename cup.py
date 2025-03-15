490117b78f7fab5d82f480f217db6bd866cc7667
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-03-15 16:21:30 +0000

    next exercise

--- cup.py
+++ cup.py
@@ -1,11 +1,17 @@
 def solve(a: int, b: int) -> None:
+    
+    result = 0
     if a >= b:
-        print("Error: 'a' must be less than 'b'")
-    else:
-        for i in range(a, b):
-            print(i)
+        a, b = b, a
+
+    for i in range(a, b):
+        num_str = str(i)
+        if len(num_str) == len(set(num_str)):
+            result += 1
+    return result
 
 a = 100
-b = 1000
+b = 1000 
 
-solve(a, b)
+result = solve(a, b)
+print(result)
