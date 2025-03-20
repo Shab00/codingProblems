3337169410813520233d8e5dda6342c4612368b1
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-03-20 10:25:04 +0000

    added  binary search

---
+++ bs.py
@@ -0,0 +1,22 @@
+def bs(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+
+result2 = bs(my_list, 3)
+result1 = bs(my_list, -1)
+
+print(result2)
+print(result1)
