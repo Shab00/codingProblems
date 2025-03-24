df59d1828b20dbd580ae35ae2cbe8a90c2a683ca
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-03-24 22:41:41 +0000

    finished wa

--- wa.py
+++ wa.py
@@ -2,17 +2,26 @@ def solve(s:str) -> str:
     if len(s) < 7:
         return s
     l = []
-    count = 0
     slit = list(s)
-    for i in range(len(slit)):
-        if i == slit[0] or i == slit[-1]:
-            print(slit[count])
-            count += 1
+    for i, char in enumerate(slit):
+        if i == 0 or i == len(slit) - 1:
+            l.append(char)
+    l.insert(1, len(slit) - 2)
+    result = "".join([str(x) for x in l])
+    return result
+
+def op(s:str) -> str:
+    if len(s) < 7:
+        return s
+    return f"{s[0]}{len(s) - 2}{s[-1]}"
 
 s = "optimization"
 w = "word"
 resultTwo = solve(w)
 result = solve(s)
-
+opr = op(w)
+oprt = op(s)
 print(resultTwo)
 print(result)
+print(opr)
+print(oprt)
