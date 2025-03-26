6d1d9455a0dc1d77bd5c3e44d7d058bd6d3154c9
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-03-26 22:37:49 +0000

    added more

--- add.py
+++ add.py
@@ -14,10 +14,18 @@ def recount(lst: list[int]) -> int:
         return 0
     return 1 + recount(lst[1:])
 
+def mnum(lst: list[int]) -> int:
+    if len(lst) == 2:
+        return lst[0] if lst[0] > lst[1] else lst[1]
+    submax = max(lst[1:])
+    return lst[0] if lst[0] > submax else submax
+
 l = [1, 2, 3, 4, 5]
 res = resum(l)
 result = sum(l)
 r = recount(l)
+m = mnum(l)
+print(m)
 print(r)
 print(res)
 print(result)
