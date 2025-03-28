67c43e5a82eb1e00e67dc9d7122979e051399e23
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-03-28 15:24:18 +0000

    fin one filp game

--- oneFlipGame.py
+++ oneFlipGame.py
@@ -1,18 +1,36 @@
 from typing import List
 def solve(l:List[int])->int:
-    dic = {}
+    maxsum, orione, cursum = 0, 0, 0
+
+    if l.count(1) == len(l) and 0 not in l:
+        return len(l) - 1
+    elif l.count(0) == len(l) and 1 not in l:
+        return len(l)
+
     for i in l:
-        if i not in dic:
-            dic[i] = 1
-        else:
-            dic[i] += 1
-    mdic = max(dic.values())
-    return mdic
+        if i == 1:
+            cursum -= 1
+            orione += 1
+        elif i == 0:
+            cursum += 1
+        
+        if cursum < 0:
+            cursum = 0
 
+        maxsum = max(maxsum, cursum)
+       
+    return orione + maxsum
 
+lsts =[ 
+       [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 
+       [1,0,0,1,0],
+       [1, 0, 0, 0, 1, 0, 0, 0], 
+       [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+       [0,1,0,1,0,1,0,1,0,1,1,0,1,1,0,1,1,0]
+]
 
-l = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+r = [22, 4, 7, 18, 11]
 
-result = solve(l)
-
-print(result)
+for i, j in zip(lsts, r):
+    result = solve(i)
+    print("%d : %d*" % (result, j))
