a9a7e0488c3c15a9e2365a245f5d870bdd5fd882
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-04-04 21:27:09 +0100

    fin solve trio

--- problem-solve-trio.py
+++ problem-solve-trio.py
@@ -2,18 +2,21 @@ from typing import List
 
 def solve(problems: List[List[int]]) -> int:
 
-    probSum = 0
-    result = []
+        count = 0
+        result = []
 
-    for sublist in problems:
-        result += sublist
-        for i in sublist:
-            probSum += i
-    print(result)
-    return probSum
+        for sublist in problems:
+            if sum(sublist) > 1:
+                count += 1
+        return count
 
-inputs = [[[ 1, 0, 1], [1, 1, 1], [0, 0, 0]],
-          [[0, 1, 1], [0, 0, 1], [1, 1, 1], [1, 1, 0]]]
+inputs = [[[1,1,0],[1,1,1],[1,0,0]],
+          [[1,0,0],[0,1,1]],
+          [[1,0,0]],
+          [[1,0,0],[0,1,0],[1,1,1],[0,0,1],[0,0,0]],
+          [[0,1,0],[0,1,0],[1,1,0],[1,1,0],[1,0,0],[0,0,1],[0,1,1],
+           [1,1,1],[1,1,0],[0,0,0],[0,0,0]]
+         ]
 
 for list in inputs:
     result = solve(list)
