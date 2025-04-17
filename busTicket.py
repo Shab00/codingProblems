99afc0a84591bf91e553d23acbe1d529e88a739c
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-04-17 20:24:31 +0100

    fin bus ticket

--- busTicket.py
+++ busTicket.py
@@ -1,14 +1,17 @@
 def solve(n:int, m:int, a:int, b:int)->int:
     print(f"n: {n} m: {m}, a: {a}, b: {b}")
+
     totalCost = n * a
     fullSp = n // m
     totalSp = fullSp * b
+    
+    leftOver = n % m
+    
+    totalSp += min(b, leftOver * a)
+    
+    return min(totalCost, totalSp)
 
-    remain = fullSp * m
-
-
-
-n, m, a, b, = 10, 3, 1, 2
+n, m, a, b, = 10, 3, 5, 1
 
 result = solve(n, m, a, b)
 
