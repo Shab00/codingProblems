daa113c696daed9c0a115d46fb51f0b7735cd76f
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-04-28 20:07:04 +0100

    started cracking combo

---
+++ crackingCombo.py
@@ -0,0 +1,9 @@
+def solve(original:int, target:int) -> int:
+    print(f"priginal: {original}, target: {target}")
+    
+
+
+original, target = 82195, 64723
+
+result = solve(original, target)
+print(result)
