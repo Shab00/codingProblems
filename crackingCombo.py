ce7b0886d40392b4ae2b6a22b16a1c6d9225967c
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-04-29 13:55:24 +0100

    fin cracking combo

--- crackingCombo.py
+++ crackingCombo.py
@@ -1,9 +1,21 @@
 def solve(original:int, target:int) -> int:
     print(f"priginal: {original}, target: {target}")
-    
+    if original == 00000:
+        sori = '00000'
+        star = str(target)
+    else:
+        sori = str(original)
+        star = str(target)
+    spins = 0
+    for i, j in zip(sori, star):
+        diff = abs(int(i) - int(j))
+        maximum = 10 - diff
 
+        spins += min(diff, maximum)
 
-original, target = 82195, 64723
+    return spins
+
+original, target = 00000, 93199
 
 result = solve(original, target)
 print(result)
