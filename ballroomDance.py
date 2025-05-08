bff02578ff54ec5252e8d68fa1c57687df156db3
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-08 11:57:34 +0100

    still working on ballroom dance

--- ballroomDance.py
+++ ballroomDance.py
@@ -2,7 +2,19 @@ from typing import List
 
 def solve(boys:List[int], girls:List[int]) -> int:
     print(f"boys:{repr(boys)}, girls:{repr(girls)}")
-    pass
+    count = 0
+    while boys:
+        for i in girls:
+            partners = abs(boys[0] - i)
+            if partners == 1:
+                count += 1
+        boys.pop(0)
+
+    return count
+
+
+
+
 
 boys, girls = [1, 4, 6, 2], [5, 1, 5, 7, 9]
 
