2e88189aff322842153ca2717c6b6da105759865
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-09 00:34:12 +0100

    fin ballroom

--- ballroomDance.py
+++ ballroomDance.py
@@ -5,10 +5,21 @@ def solve(boys:List[int], girls:List[int]) -> int:
     girls.sort()
     print(f"boys:{repr(boys)}, girls:{repr(girls)}")
     count = 0
-    i = boys[0]
-    j = girls[0]
-    while i < j:
-        
+    i = 0
+    j = 0
+    while i < len(boys) and j < len(girls):
+
+        comp = abs(boys[i] - girls[j])
+
+        if comp <= 1:
+            count += 1
+            i += 1
+            j += 1
+        elif comp > 1:
+            if boys[i] < girls[j]:
+                i += 1
+            else:
+                j += 1
     return count
 
 
