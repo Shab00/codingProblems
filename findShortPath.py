c065a73646b69194ee6b23dd0d0e998aa2d9c159
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-12 11:42:38 +0100

    added bfs dfs maze

---
+++ findShortPath.py
@@ -0,0 +1,13 @@
+def solve(maps: list) -> n: list:
+    pass
+
+
+maze = [
+    ["S", 0, 1, 0, "E"],
+    [0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 0, 1]
+]
+
+result = solve(maze)
+print(maze)
