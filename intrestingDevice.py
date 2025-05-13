81a7ade430db44fbff5b28862c0135e8768fa17d
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-13 12:50:50 +0100

    fixed error and fin intresting device

--- intrestingDevice.py
+++ intrestingDevice.py
@@ -1,17 +1,20 @@
 def solve(n:int, m:int)->int:
-
     count = 0
-    while m > n: 
+    while m > n:
         if m % 2 == 0:
-            m //= 2
+                m //= 2
         else:
-            m -= 1
+            m += 1
         count += 1
-    return count + (n - m) 
+    count += n - m  
+    return count
+
+
+
 
 nAndM = [[10, 1], [2, 1], [99, 100], [666, 6666], [6666, 666]] 
 
 for i in nAndM:
     n, m = i 
     result = solve(n, m)
-    print(f"n = {n}, m = {m}, result = {result}")
+    print(result)
