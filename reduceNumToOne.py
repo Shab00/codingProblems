91dfb7130622388282da1f37fe68df7be53412ac
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-14 14:02:33 +0100

    finished reduce num to one

--- reduceNumToOne.py
+++ reduceNumToOne.py
@@ -1,11 +1,14 @@
 def solve(n:int)->int:
-
+    count = 0
     while n > 1:
         if n % 6 == 0:
             n //= 6
         elif n % 2 == 0:
             return -1
-    return n
+        else:
+            n *= 2
+        count += 1
+    return count
 
 inputs = [1, 2, 48, 108, 216, 36, 18]
 
