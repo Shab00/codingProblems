cdbc6af8da9540392ae5f6fe749e8ecc6d351e0c
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-23 10:23:15 +0100

    finished shortest path bfs

--- shortPathInGrid.py
+++ shortPathInGrid.py
@@ -1,7 +1,25 @@
 from collections import deque
 
 def solve(maze: list, start: tuple, end: tuple) -> int:
-    pass
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    path = deque()
+    path.append((start, [start]))
+    visted = set()
+    visted.add(start)
+    while path:
+        (curRow, curCol), pathSoFar = path.popleft()
+        if (curRow, curCol) == end:
+            return len(pathSoFar) - 1
+        for dr, dc in directions:
+            nr, nc = curRow + dr, curCol + dc
+            if (0 <= nr < len(maze) and
+                0 <= nc < len(maze[0]) and 
+                maze[nr][nc] != 1 and 
+                (nr, nc) not in visted):
+                path.append(((nr, nc), pathSoFar + [(nr, nc)]))
+                visted.add((nr, nc))
+
+    return -1
 
 maze = [
         [0, 1, 0, 0, 0],
