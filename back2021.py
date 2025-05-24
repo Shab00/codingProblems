ccdd01eca875a91aff326470bf7b14147c6ca173
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-24 11:06:50 +0100

    fin back2021.py

--- back2021.py
+++ back2021.py
@@ -1,8 +1,15 @@
 def solve(n:int)->str:
-    pass
 
+    b = n // 2021
+
+    for i in range(b + 1):
+
+        dif = n - (2021 * i)
+        if dif % 2020 == 0:
+            return "YES"
+    return "NO"
 inputs = [4041, 4042, 8081, 2019, 4053, 3031]
 
 for i in inputs:
-    result = solve(inputs)
+    result = solve(i)
     print(result)
