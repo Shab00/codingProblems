94c7b2163aa1a6e4c3c026730f8f8f75aee8906b
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-25 18:55:59 +0100

    added fantaDrink

---
+++ fantaDrink.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def solve(prices:List[int], coins:List[int])->List[int]:
+    pass
+
+prices, coins = [3, 10, 8, 6, 11], [1, 10, 3, 11]
+
+result = solve(prices, coins)
+print(result)
