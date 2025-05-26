5a6b608b6d994aafd75fac6e0718608feaf8e30d
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-26 10:46:37 +0100

    bfs wordladder

---
+++ wordLadder.py
@@ -0,0 +1,11 @@
+from typing import List
+def solve(beginWord:str, endWord:str, wordList:List[str])->int:
+    pass
+
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log","cog"]
+
+result = solve(beginWord, endWord, wordList)
+
+print(result)
