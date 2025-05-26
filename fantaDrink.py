d3f7e84c30d6c52eb59f5f8983acb8e8f6df57b8
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-26 21:58:11 +0100

    fim fanta drink  but shouldve been binary search

--- fantaDrink.py
+++ fantaDrink.py
@@ -2,12 +2,29 @@ from typing import List
 
 def solve(prices:List[int], coins:List[int])->List[int]:
 
-    prices.sort()
-    coins.sort()
+    pl, lp = 0, len(prices) - 1 
+    pc, lc = 0, len(coins) - 1
+    count = 0 
+    output = []
 
-    p, c = 0, 0
+    while pl <= lp and pc <= lc:
+        
+        if pl == lp:
+            if coins[pc] >= prices[pl]:
+                count += 1
+            output.append(count)
+            count = 0
+            pl = 0
+            pc += 1
 
-    
+        elif coins[pc] >= prices[pl]:
+            count += 1
+            pl += 1
+
+        else:
+            pl += 1
+
+    return output
 
 prices, coins = [3, 10, 8, 6, 11], [1, 10, 3, 11]
 
