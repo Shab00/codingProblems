2145106cdf1c9b0888b306bb44b5a84b2dc23841
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-28 20:10:14 +0100

    finished word ladder

--- wordLadder.py
+++ wordLadder.py
@@ -1,7 +1,29 @@
 from typing import List
+from collections import deque
 def solve(beginWord:str, endWord:str, wordList:List[str])->int:
-    pass
 
+    dictionary = set(wordList)
+    path = deque()
+    path.append((beginWord, 1))
+    visted = set()
+    visted.add(beginWord)
+    
+    while path:
+        curWord, step = path.popleft()
+        if curWord == endWord:
+            return step
+        for word in dictionary:
+            defrence = 0            
+            for a, b in zip(curWord, word):
+                if a != b:
+                    defrence += 1
+                    
+            if defrence == 1 and word not in visted:
+                path.append((word, step + 1))
+                visted.add(word)
+    return 0
+        
+    
 beginWord = "hit"
 endWord = "cog"
 wordList = ["hot","dot","dog","lot","log","cog"]
