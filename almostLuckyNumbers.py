cda712f515d8b18834a9f2d97084c167d54ff21e
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-05-29 21:46:56 +0100

    almostlucky number finished

--- almostLuckyNumbers.py
+++ almostLuckyNumbers.py
@@ -1,6 +1,13 @@
 def solve(n:int)->str:
 
-    pass
+    luckyNumbers = []
+    for i in range(1, n + 1):
+        if all(d in {'4', '7'} for d in str(i)):
+            luckyNumbers.append(i)
+    for i in luckyNumbers:
+        if n % i == 0:
+            return "YES"
+    return"NO"
 
 inputs = [47, 16, 78, 28, 444, 50, 74, 99]
 for i in inputs:
