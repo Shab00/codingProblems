2313a7ae8aabd642015508cc2af6d2596b133b1c
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-06-03 15:56:11 +0100

    fin bintreemaxdepth startred leaves

--- binaryTreeMaxDepth.py
+++ binaryTreeMaxDepth.py
@@ -11,7 +11,19 @@ root.right.left = TreeNode(15)
 root.right.right = TreeNode(7)
 
 def solve(root: TreeNode) -> int:
-    pass
+    if not root:
+        return 0
+
+    stack = [(root, 1)]
+    max_depth = 0
+    while stack:
+        node, depth = stack.pop()
+        if node:
+            max_depth = max(max_depth, depth)
+            stack.append((node.left, depth + 1))
+            stack.append((node.right, depth + 1))
+
+    return max_depth
 
 result = solve(root)
 print(result)
---
+++ countLeaves.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val     
+        self.left = left    
+        self.right = right 
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+def count_leaves(root: TreeNode) -> int:
+    pass
+
+result = count_leaves(root)
+print(result) 
