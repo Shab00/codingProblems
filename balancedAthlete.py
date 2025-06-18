2e2b21720e9f87662277fe0d0d31bc41ea128ec1
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-06-18 18:09:38 +0100

    finished balanced athletes

--- balancedAthlete.py
+++ balancedAthlete.py
@@ -1,7 +1,14 @@
 from typing import List
 
 def solve(athletes:List[int]) -> int:
-    pass
+
+    athletes.sort()
+    results = []
+    for i in range(1, len(athletes)):
+        teama = athletes[:i]
+        teamb = athletes[i:]
+        results.append(min(teamb) - max(teama))
+    return min(results)
 
 inputs = [3,1,2,6,4]
 
