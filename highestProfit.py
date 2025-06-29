c6a009f39c77520987a4aac42f6ce668343fdfb9
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-06-29 22:17:50 +0100

    still can not figure out highest profit

--- highestProfit.py
+++ highestProfit.py
@@ -1,31 +1,30 @@
 def solve(ls: list) -> int:
     l, r = 0, 1
+    maxP = 0
     profit = 0
     while r < len(ls):
 
-        if ls[l] > ls[r]:
-            l += 1
-            r += 1
-        elif ls[l] < ls[r]:
-            profit += ls[r] - ls[l]
-            l += 1
-            r += 1
+        if ls[l] < ls[r]:
+            profit = ls[r] - ls[l]
+            maxP = max(maxP, profit)
         else:
-            l += 1
-            r += 1
-    return profit
+            l = r
+        r += 1
+
+    return maxP
 
 def greedy(ls: list) -> int:
+
     profit = 0
     stack = []
-    for day in range(len(ls) - 1): 
-        if ls[day] < ls[day + 1]:
-            stack.append(ls[day])
-        if ls[day] > ls[day + 1] and stack:
-            profit += ls[day] - stack.pop(0)
-    if stack:
-        for p in stack:
-            profit += ls[-1] - p
+    n = len(ls)
+    for i in range(n - 1):
+        if ls[i] < ls[i + 1]:
+            stack.append(ls[i])  
+        if ls[i] > ls[i + 1] and stack:
+            profit += ls[i] - stack.pop(0)  
+    for buy_price in stack:
+        profit += ls[-1] - buy_price
     return profit
 
 inputs = [
