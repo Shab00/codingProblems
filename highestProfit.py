6e9aba31ba1248c487fcc2562502136a6cceaa78
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-06-29 21:30:27 +0100

    higestProfit did not solve!

--- highestProfit.py
+++ highestProfit.py
@@ -1,5 +1,32 @@
 def solve(ls: list) -> int:
-    pass
+    l, r = 0, 1
+    profit = 0
+    while r < len(ls):
+
+        if ls[l] > ls[r]:
+            l += 1
+            r += 1
+        elif ls[l] < ls[r]:
+            profit += ls[r] - ls[l]
+            l += 1
+            r += 1
+        else:
+            l += 1
+            r += 1
+    return profit
+
+def greedy(ls: list) -> int:
+    profit = 0
+    stack = []
+    for day in range(len(ls) - 1): 
+        if ls[day] < ls[day + 1]:
+            stack.append(ls[day])
+        if ls[day] > ls[day + 1] and stack:
+            profit += ls[day] - stack.pop(0)
+    if stack:
+        for p in stack:
+            profit += ls[-1] - p
+    return profit
 
 inputs = [
         [10, 5, 4, 7, 9, 12, 6, 2, 10],
@@ -11,5 +38,5 @@ inputs = [
         ]
 
 for i in inputs:
-    result = solve(i)
+    result = greedy(i)
     print(result)
