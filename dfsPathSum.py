b7f10c96ab1b91a425408cd57e41f7f5e1e44103
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-07-05 11:01:59 +0100

    started dfsPathSum

---
+++ dfsPathSum.py
@@ -0,0 +1,25 @@
+class TreeNode:
+    def __init__(self,val=0,left=None,right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+root = TreeNode(5)
+root.left = TreeNode(4)
+root.left.left = TreeNode(11)
+root.left.left.left = TreeNode(7)
+root.left.left.right = TreeNode(2)
+root.right = TreeNode(8)
+root.right.left = TreeNode(13)
+root.right.right = TreeNode(4)
+root.right.right.right = (1)
+
+def pathSum(root: TreeNode, target: int):
+
+    pass
+
+target = [22, 26, 18]
+
+for i in target:
+    result = pathSum(root, i)
+    print(result)
