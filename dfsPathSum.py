a22d3aa0681ea6b9b217dd296eab51b7df977b8f
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-07-05 13:03:09 +0100

    finished dfs path

--- dfsPathSum.py
+++ dfsPathSum.py
@@ -12,14 +12,37 @@ root.left.left.right = TreeNode(2)
 root.right = TreeNode(8)
 root.right.left = TreeNode(13)
 root.right.right = TreeNode(4)
-root.right.right.right = (1)
+root.right.right.right = TreeNode(10)
+
+def treeList(root: TreeNode, path: None):
+
+    if path is None:
+        path = []
+    if not root:
+        return
+    path.append(root.val)
+    if not root.left and not root.right:
+        print(path)
+    else:
+        treeList(root.left, path[:])
+        treeList(root.right, path[:])
+
 
 def pathSum(root: TreeNode, target: int):
+    if not root:
+        return False
 
-    pass
+    if not root.left and not root.right:
+        return root.val == target
+
+    return (pathSum(root.left, target - root.val) or
+            pathSum(root.right, target - root.val))
 
 target = [22, 26, 18]
 
 for x in target:
     result = pathSum(root, x)
     print(result)
+
+result = treeList(root, [])
+print(result)
