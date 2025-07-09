a9640729a593fcd38a94332c20edce19b1add897
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-07-09 20:10:36 +0100

    finished word compression

--- wordsCompression.py
+++ wordsCompression.py
@@ -1,10 +1,20 @@
 def solve(str: str) -> str:
     
     ls = str.split()
-    result = [] 
-    for word in ls:
-        result.append(word)
-    return result
+    result = []
+
+
+    for i in range(len(ls) - 1):
+        k = min(len(ls[i]), len(ls[i + 1]))
+        first = ls[i]
+        second = ls[i + 1]
+        max_overlap = 0
+        for i in range(k + 1):
+            if first[-i:] == second[:i]:
+                max_overlap = i
+        result.append(second[max_overlap:])
+
+    return ls[0] + ''.join(result)
 
 inputs = ["cat attack", 
           "football ball alley", 
