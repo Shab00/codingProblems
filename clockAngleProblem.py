b22fc6fc10c83c19e793075db669b15802a6d89a
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-07-14 11:44:21 +0100

    started clock angle problem

---
+++ clockAngleProblem.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def solve(target_angle: int) -> List:
+    H = ((5.5) *)
+
+inputs = [(120,[4, 0]), (200,[2, 40])]
+
+for i, expected in inputs:
+    result = solve(i)
+    print(f"Inputs: {i} | Output: {result} | Expected: {expected}")
