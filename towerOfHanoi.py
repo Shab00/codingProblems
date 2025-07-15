26e52369f9e1511a4b46b3857a545672537f018b
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-07-15 23:24:40 +0100

    finished tower of hanoi

--- towerOfHanoi.py
+++ towerOfHanoi.py
@@ -1,16 +1,20 @@
 def solve(n: int) -> list[list]:
+    
     def move(num: int, sou: int, dest: int, aux: int, ml: list):
 
         if num == 1:
             ml.append([sou, dest])
             return
-
+        else:
+            move(num - 1, sou, aux, dest, ml)
+            ml.append([sou, dest])
+            move(num - 1, aux, dest, sou, ml)
 
     ml = []
     move(n, 0, 2, 1, ml)
     return ml
 
-inputs = [1, 2, 3]
+inputs = [1, 2, 3, 8]
 for i in inputs:
     result = solve(i)
     print(result)
