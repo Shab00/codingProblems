cd3482d821dd215866bb2439cdac40e95deeffa2
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-07-17 18:18:02 +0100

    finished roman int

--- leetcode/romanToIntegr.py
+++ leetcode/romanToIntegr.py
@@ -4,14 +4,19 @@ def romanToInt(s: str) -> int:
                  "L": 50, "C": 100, "D": 500,
                  "M": 1000}
     count = 0
-    for i in range(len(s) - 1):
-        print("look %i" % romanDict[s[i]])
-        if romanDict[s[i]] < romanDict[s[i + 1]]:
-            print(s[i])
-            count += romanDict[s[i]]
+    l, r = 0, 1
+    while r < len(s):
+        if romanDict[s[l]] < romanDict[s[r]]:
+            subtract = romanDict[s[r]] - romanDict[s[l]]
+            count += subtract
+            l += 2
+            r += 2
         else:
-            count += romanDict[s[i]]
-
+            count += romanDict[s[l]]
+            l += 1
+            r += 1
+    if l < len(s):
+        count += romanDict[s[l]]
     return count
 
 inputs = [("III", 3), ("LVIII", 58), ("MCMXCIV", 1994)]
