a25bb8155604625e2ffa83c614b792306498b1b2
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-07-17 10:46:49 +0100

    started roman numerals problem

--- leetcode/romanToIntegr.py
+++ leetcode/romanToIntegr.py
@@ -1,5 +1,22 @@
 def romanToInt(s: str) -> int:
-    pass
+
+    romanDict = {"I": 1, "V": 5, "X": 10,
+                 "L": 50, "C": 100, "D": 500,
+                 "M": 1000}
+    count = 0
+    l, r = 0, 1 
+    for i in range(len(s) - 1):
+        if romanDict[s[i]] < romanDict[s[i + 1]]:
+            print(s[i], print(s[i]))
+            l += 1
+            r += 1
+            count += romanDict[s[l]]
+        else:
+            count += romanDict[s[l]]
+            l += 1
+            r += 1
+
+    return count
 
 inputs = [("III", 3), ("LVIII", 58), ("MCMXCIV", 1994)]
 
