ef5831ba7f8a308d462977a56b2d3499266b565a
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-07-20 17:02:10 +0100

    need to return it as a listnode

--- leetcode/addTwoNumbers.py
+++ leetcode/addTwoNumbers.py
@@ -14,15 +14,21 @@ l2.next.next = ListNode(4)
 def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
 
     current1, current2 = l1, l2
-    result = []
     temp1, temp2 = [], []
     while current1 and current2:
         temp1.append(str(current1.val))
         temp2.append(str(current2.val))
         current1 = current1.next
         current2 = current2.next
-    print(temp1, temp2)
-    return result
+    temp1.reverse()
+    temp2.reverse()
+    num1 = ''.join(temp1)
+    num2 = ''.join(temp2)
+    total = int(num1) + int(num2)
+    strTotal = str(total)
+    l3 = list(strTotal)
+    l3.reverse()
+    return l3
 
 result = addTwoNumbers(l1, l2)
 print(result)
