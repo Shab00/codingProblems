f9f97da1d7afee45bb205412ddc9cd0512a2a1db
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-07-24 21:16:56 +0100

    fixed with odd the even

--- leetcode/longestPalString.py
+++ leetcode/longestPalString.py
@@ -1,25 +1,45 @@
 def longestPalString(s: str) -> str:
     if not s:
         return ""
-    mid = len(s) // 2
-    if len(s) % 2 == 0:
-        l, r = mid, mid - 1
-    else:
-        l, r = mid, mid
-    result = []
-    while l >= 0 and r <= len(s):
-        tempList = s[l:r + 1]
-        tempResult = []
-        for i in tempList:
-            tempResult.append(i)
-            result.append(''.join(tempResult))
-        l -= 1
-        r += 1
-    new = []
-    for i in result:
-        if i[0] == i[len(i) - 1] and len(i) > 1:
-            new.append(i)
-    return new[0]
+    result = ""
+    length = len(s)
+    for center in range(len(s)):
+        l, r = center, center
+        while l >= 0 and r < length and s[l] == s[r]:
+            if (r - l + 1) > len(result):
+                result = s[l:r+1]
+            l -= 1
+            r += 1
+        l, r = center, center + 1
+        while l >= 0 and r < length and s[l] == s[r]:
+            if (r - l + 1) > len(result):
+                result = s[l:r+1]
+            l -= 1
+            r += 1
+    return result
+        
+
+#     if not s:
+        # return ""
+    # mid = len(s) // 2
+    # if len(s) % 2 == 0:
+        # l, r = mid, mid - 1
+    # else:
+        # l, r = mid, mid
+    # result = []
+    # while l >= 0 and r <= len(s):
+        # tempList = s[l:r + 1]
+        # tempResult = []
+        # for i in tempList:
+            # tempResult.append(i)
+            # result.append(''.join(tempResult))
+        # l -= 1
+        # r += 1
+    # new = []
+    # for i in result:
+        # if i[0] == i[len(i) - 1] and len(i) > 1:
+            # new.append(i)
+    # return new[0]
 
 inputs = [("babad", "bab"), ("cbbd", "bb")]
 for i, o in inputs:
