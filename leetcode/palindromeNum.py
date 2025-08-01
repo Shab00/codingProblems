d3803d510fb7e41222d487224854da62e1b8804a
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-08-01 16:19:51 +0100

    solved using math

--- leetcode/palindromeNum.py
+++ leetcode/palindromeNum.py
@@ -14,13 +14,30 @@ def isPal(x: int) -> bool:
             result.append(strNum[l])
         l -= 1
         r += 1
-    print(result)
     if result:
         return True
+
+def math(x: int) -> bool:
+    if x < 0:
+        return False
+    opp = 0
+    copy = x
+    while x != 0:
+        digit = x % 10
+        opp = opp * 10 + digit
+        x //= 10
+    if opp == copy:
+        return True
+    else:
+        return False
 
 inputs = [(121, True), (-121, False), (10, False), 
           (123321, True), (-10, False)]
 
 for nums, b in inputs:
     result = isPal(nums)
+    print(f"input: {nums} || output: {result} || expected: {b}")
+
+for nums, b in inputs:
+    result = math(nums)
     print(f"input: {nums} || output: {result} || expected: {b}")
