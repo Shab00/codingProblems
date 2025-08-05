18cec76f01a2599d618735dd37b11a493fd1913c
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-08-05 11:30:17 +0100

    finished intToRoman ordered list of tuples

--- leetcode/intToRoman.py
+++ leetcode/intToRoman.py
@@ -1,5 +1,28 @@
 def intToRoman(num: int) -> str:
-    pass
+
+    pairs = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+            ]
+    result = ""
+    for value, char in pairs:
+        while num >= value:
+            result += char
+            num -= value
+
+    return result
+
 
 inputs = [(3749, "MMMDCCXLIX"), (58, "LVIII"), 
           (1994, "MCMXCIV")]
