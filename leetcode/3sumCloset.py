8739b037957b122ac3c920dd6e7c9f61a0482835
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-08-12 17:13:23 +0100

    finished 3 sum closet

--- leetcode/3sumCloset.py
+++ leetcode/3sumCloset.py
@@ -1,18 +1,15 @@
 def threeSum(nums: list[int], target: int) -> int:
     nums.sort()
-    result = 0
+    result = float('inf')
     for index in range(len(nums)):
         l, r = index + 1, len(nums) - 1
         while l < r:
             tNum = nums[index] + nums[l] + nums[r]
-            if tNum < 0:
-                tNum = tNum * -1
             dif = abs(tNum - target)
-            print(tNum, dif)
             if tNum == target:
                 return tNum
-            else:
-                result = min(result, tNum)
+            elif abs(tNum - target) < abs(result - target):
+                result = tNum
 
             if tNum < target:
                 l += 1
@@ -20,7 +17,9 @@ def threeSum(nums: list[int], target: int) -> int:
                 r -= 1
     return result
 
-inputs = [([-1,2,1,-4], 1, 2), ([0,0,0], 1, 0)]
+inputs = [([-1,2,1,-4], 1, 2), 
+          ([0,0,0], 1, 0),
+          ([1,1,1,0], 100, 3)]
 for i, t, e in inputs:
     result = threeSum(i, t)
     print(f"inputs: {i, t} || outputs: {result} || expected: {e}")
