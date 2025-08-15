14f9e14cb63f4bbe8c2a4fa6eca4a8e820954ea3
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-08-15 11:46:11 +0100

    found target but not multiples

--- aiCodingQuestions/comboSumTarget.py
+++ aiCodingQuestions/comboSumTarget.py
@@ -1,5 +1,30 @@
 def comboSum(can: list[int], target: int) -> list[list[int]]:
-    pass
+
+    if not can:
+        return []
+
+    sol = []
+    ret = []
+    n = len(can)
+
+    def backtrack(i=0):
+        if i == n:
+            total = sum(sol[:])
+            if total == target:
+
+                ret.append(sol[:])
+            return
+
+        backtrack(i+1)
+
+        sol.append(can[i])
+        backtrack(i+1)
+        sol.pop()
+    backtrack()
+    return ret
+
+
+
 
 inputs = [([2], 4),([2,3,6,7],7), ([2,3,5], 8)]
 for i in inputs:
