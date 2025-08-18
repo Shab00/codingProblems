e05821d80a72b6e42c4f3302b9c861341bb065ba
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-08-18 14:06:28 +0100

    4 sum still working on

--- leetcode/4sum.py
+++ leetcode/4sum.py
@@ -1,5 +1,18 @@
 def my_sum(nums:list[int], target: int) -> list[list[int]]:
-    pass
+    result = []
+    nums.sort()
+    for first in range(len(nums)):
+        for second in range(first + 1, len(nums)):
+            l, r = second + 1, len(nums) - 1
+
+            while l < r:
+                cur =  nums[first] + nums[second] + nums[l] + nums[r]
+                if cur == target:
+                    result.append([nums[first], nums[second], nums[l], nums[r]])
+                if cur < target:
+                    l += 1
+                r -= 1
+    return result
 
 inputs = [([1,0,-1,0,-2,2], 0, [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]),
           ([2,2,2,2,2], 8, [[2,2,2,2]])]
