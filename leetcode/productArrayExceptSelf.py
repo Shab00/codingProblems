a3434c3852e902209ed63c6590427cca578c4432
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-08-23 19:51:43 +0100

    solved product array but too slow

--- leetcode/productArrayExceptSelf.py
+++ leetcode/productArrayExceptSelf.py
@@ -1,9 +1,29 @@
 def product(nums: list[int]) -> list[int]:
-    pass
+
+    p = 0
+    arr = len(nums) - 1
+    result = []
+    while p <= arr:
+        cur = []
+        for i, num in enumerate(nums):
+            if i == p:
+                continue
+            else:
+                cur.append(num)
+        sums = 1
+        for num in cur:
+            sums *= num
+        result.append(sums)
+        p += 1
+    return result
+
+
+
 
 inputs = [
         ([1,2,3,4], [24,12,8,6]), 
-        ([-1,1,0,-3,3], [0,0,9,0,0])
+        ([-1,1,0,-3,3], [0,0,9,0,0]),
+        ([0,0], [0,0])
          ]
 
 for i, e in inputs:
