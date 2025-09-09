c6c00bc9695ce301ce79e97127056f071be118ca
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-09-09 22:24:07 +0100

    solved with backtracking had help, will solve with stack next

--- leetcode/generatePrenthesis.py
+++ leetcode/generatePrenthesis.py
@@ -1,20 +1,20 @@
 def generate(n: int) -> list[str]:
-    
-    leftBrace = '('
-    rightBrace = ')'
 
     sol = []
     ret = []
-
-    def backtrack(i):
-        if i == n:
+    def backtrack(left_count, right_count):
+        if len(sol) == 2 * n:
             ret.append("".join(sol))
             return
-
-        sol.append(leftBrace)
-        backtrack(i+1)
-        sol.pop()
-    backtrack(0)
+        if left_count < n:
+            sol.append('(')
+            backtrack(left_count + 1, right_count)
+            sol.pop()
+        if right_count < left_count:
+            sol.append(')')
+            backtrack(left_count, right_count + 1)
+            sol.pop()
+    backtrack(0, 0)
     return ret
 
 inputs = [(3, ["((()))","(()())","(())()","()(())","()()()"]),
