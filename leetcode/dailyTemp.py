5f0cb2579be5bbbfdab7c3b35dd53e429b1116e6
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-09-10 17:45:19 +0100

    bruteforce dailytemp

--- leetcode/dailyTemp.py
+++ leetcode/dailyTemp.py
@@ -1,5 +1,21 @@
 def dailyTemp(temp: list[int]) -> list[int]:
-    pass
+    l = []
+    for i in range(len(temp)):
+        p = i + 1
+        count = 0
+        while p < len(temp):
+            if temp[i] < temp[p]:
+                count += 1
+                l.append(count)
+                count = 0
+                p += 1
+                break
+            else:
+                count += 1
+                p += 1
+        else:
+            l.append(0)
+    return l
 
 inputs = [([73,74,75,71,69,72,76,73], [1,1,4,2,1,1,0,0]),
           ([30,40,50,60], [1,1,1,0]),
