7716000ab3be4ec671a266b332014e4e31c2e97c
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-09-10 22:27:04 +0100

    solving using stack and counting indices

--- leetcode/dailyTemp.py
+++ leetcode/dailyTemp.py
@@ -1,4 +1,4 @@
-def dailyTemp(temp: list[int]) -> list[int]:
+def brute(temp: list[int]) -> list[int]:
     l = []
     for i in range(len(temp)):
         p = i + 1
@@ -16,6 +16,9 @@ def dailyTemp(temp: list[int]) -> list[int]:
         else:
             l.append(0)
     return l
+
+def dailyTemp(temp: list[int]) -> list[int]:
+    pass
 
 inputs = [([73,74,75,71,69,72,76,73], [1,1,4,2,1,1,0,0]),
           ([30,40,50,60], [1,1,1,0]),
