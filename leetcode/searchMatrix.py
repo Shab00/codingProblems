6f8545b7f18f332ae7287634039f75075d240568
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-09-17 21:01:54 +0100

    solved log m*n

--- leetcode/searchMatrix.py
+++ leetcode/searchMatrix.py
@@ -1,19 +1,30 @@
 def search(matrix: list[list[int]], target: int) -> bool:
-    l = []
-    for rows in range(len(matrix)):
-        for cols in range(len(matrix[rows])):
-            l.append(matrix[rows][cols])  
-    print(l)
-    s, r = 0, len(l) - 1
-    while s <= r:
-        m = (s + r) // 2
-        if l[m] == target:
-            return True
-        elif l[m] > target:
-            r = m - 1
-        elif l[m] < target:
-            s = m + 1
+    for rows in matrix:
+        l, r = 0, len(rows) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if rows[m] == target:
+                return True
+            elif rows[m] > target:
+                r = m - 1
+            elif rows[m] < target:
+                l = m + 1
     return False
+    # l = []
+    # for rows in range(len(matrix)):
+        # for cols in range(len(matrix[rows])):
+            # l.append(matrix[rows][cols])  
+    # print(l)
+    # s, r = 0, len(l) - 1
+    # while s <= r:
+        # m = (s + r) // 2
+        # if l[m] == target:
+            # return True
+        # elif l[m] > target:
+            # r = m - 1
+        # elif l[m] < target:
+            # s = m + 1
+    # return False
 
 
 inputs = [([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3, True),
