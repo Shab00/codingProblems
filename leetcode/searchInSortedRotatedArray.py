6cf18816e3e6dbce29a5dd67adf61da2ce7d6423
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-09-25 08:39:25 +0100

    started search in sorted rotated array

---
+++ leetcode/searchInSortedRotatedArray.py
@@ -0,0 +1,31 @@
+def search(nums: list[int], target: int) -> int:
+
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        m = (l + r) // 2
+        difl = abs(target - nums[l])
+        difr = abs(target - nums[r])
+        print(nums[l], nums[m], nums[r])
+        if nums[m] == target:
+            return m
+        elif difl > difr:
+            l = m + 1
+        else:
+            r = m - 1
+    return -1
+
+
+inputs = [([4,5,6,7,0,1,2], 0, 4),
+          ([4,5,6,7,0,1,2], 3, -1),
+          ([1], 0, -1),
+          ([4,5,6,7,8,1,2,3], 8, 4)]
+
+for i, o, e in inputs:
+    result = search(i, o)
+    print(
+            f"inputs:\n{i, o}\n"
+            f"outputs:\n{result}\n"
+            f"expected:\n{e}\n"
+            f"{'-'*50}"
+            )
