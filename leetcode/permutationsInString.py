3f7f1051d5a5b46e88a65ce8372083c25df603c4
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-10-08 09:21:35 +0100

    finisehed permutations in string added test.py

--- leetcode/permutationsInString.py
+++ leetcode/permutationsInString.py
@@ -1,6 +1,32 @@
 def check(s1: str, s2: str) -> bool:
-    pass
 
+    hmap = {}
+    for i in s1:
+        if i not in hmap:
+            hmap[i] = 0
+        hmap[i] += 1
+
+    smap = {}
+    l = 0
+    for r in range(len(s2)):
+        char = s2[r]
+        if char not in smap:
+            smap[char] = 0
+        smap[char] += 1
+
+    
+        if r - l + 1 > len(s1):
+            lchar = s2[l]
+            smap[lchar] -= 1
+            if smap[lchar] == 0:
+                del smap[lchar]
+            l += 1
+
+        if r - l + 1 == len(s1):
+            if hmap == smap:
+                return True
+    return False
+       
 inputs = [("ab", "eidbaooo", True),
           ("ab", "eidboaoo", False)]
 
