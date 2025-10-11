e54226afb1bb8569fa1e3f73bed91944e0b3c1ac
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-10-11 10:26:55 +0100

    solved but with lots of help, need to start reading interciew question book

--- leetcode/minWindowSubstring.py
+++ leetcode/minWindowSubstring.py
@@ -1,15 +1,39 @@
 def minWindow(s: str, t: str) -> str:
-
-    key = {}
-    for i in t:
-        if i not in key:
-            key[i] = 0
-        key[i] += 1
-
-    array = {}
+    if not t or not s:
+        return ""
+    
+    need = {}
+    for c in t:
+        need[c] = need.get(c, 0) + 1
+    
+    window = {}
+    have = 0
+    need_total = len(need)
+    res = [float("inf"), 0, 0]  
     l = 0
     for r in range(len(s)):
-        
+        c = s[r]
+        window[c] = window.get(c, 0) + 1
+
+        if c in need and window[c] == need[c]:
+            have += 1
+
+        while have == need_total:
+            if (r - l + 1) < res[0]:
+                res = [r - l + 1, l, r]
+            
+            left_char = s[l]
+            window[left_char] -= 1
+            if left_char in need and window[left_char] < need[left_char]:
+                have -= 1
+            l += 1
+
+    min_len, start, end = res
+    if min_len == float("inf"):
+        return ""
+    else:
+        return s[start:end+1]
+
 
 inputs = [("OUZODYXAZV", "XYZ", "YXAZ"),
           ("xyz", "xyz", "xyz"),
