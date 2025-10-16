ca5e656368618041b6e922fbe8c26fb462b9b7be
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-10-16 19:29:19 +0100

    finished reorderlist with help

--- leetcode/reorderList.py
+++ leetcode/reorderList.py
@@ -8,8 +8,36 @@ def reorderList(head: ListNode) -> None:
     Reorders the list as described in the problem statement.
     Do not return anything, modify head in-place instead.
     """
-    pass
 
+    if not head or not head.next or not head.next.next:
+        return
+
+    slow, fast = head, head
+    while fast and fast.next:
+        prev = slow
+        slow = slow.next
+        fast = fast.next.next
+    prev.next = None
+
+    prev2 = None
+    curr = slow
+    while curr:
+        next_temp = curr.next
+        curr.next = prev2
+        prev2 = curr
+        curr = next_temp
+    second = prev2
+
+    first = head
+    while first and second:
+        tmp1 = first.next
+        tmp2 = second.next
+        first.next = second
+        if tmp1 is None:
+            break
+        second.next = tmp1
+        first = tmp1
+        second = tmp2
 
 def list_to_linkedlist(lst):
     if not lst:
