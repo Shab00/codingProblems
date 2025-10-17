19d93d9be63f9902df90452302b85bd936002a1e
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-10-17 10:29:47 +0100

    started removing nth in linked list

---
+++ leetcode/removeNthNodeList.py
@@ -0,0 +1,37 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def removeNthFromEnd(head: ListNode, n: int) -> ListNode:
+    pass
+
+
+def list_to_linkedlist(lst):
+    if not lst:
+        return None
+    head = ListNode(lst[0])
+    current = head
+    for val in lst[1:]:
+        current.next = ListNode(val)
+        current = current.next
+    return head
+
+def linkedlist_to_list(head):
+    result = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    return result
+
+test_cases = [
+    ([1,2,3,4,5], 2),
+    ([1], 1),
+    ([1,2], 1)
+]
+
+for case, p in test_cases:
+    head = list_to_linkedlist(case)
+    remove = removeNthFromEnd(head, p)  
+    output = linkedlist_to_list(remove)
+    print(f"Input: {case, p} -> Output: {output}")
