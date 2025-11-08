49675cfce272c6fc213cfba363fe4b6d4a9962f4
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-11-08 12:39:49 +0000

    add two numbers linked list

--- leetcode/addTwoNumbers.py
+++ leetcode/addTwoNumbers.py
@@ -12,31 +12,31 @@ l2.next = ListNode(6)
 l2.next.next = ListNode(4)
 
 def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
+    dummy = ListNode()
+    cur = dummy
 
-    current1, current2 = l1, l2
-    temp1, temp2 = [], []
+    carry = 0
+    while l1 or l2 or carry:
+        v1 = l1.val if l1 else 0
+        v2 = l2.val if l2 else 0
 
-    while current1:
-        temp1.insert(0, str(current1.val))
-        current1 = current1.next
+        val = v1 + v2 + carry
+        carry = val // 10
+        val = val % 10
+        cur.next = ListNode(val)
 
-    while current2:
-        temp2.insert(0, str(current2.val))
-        current2 = current2.next
+        cur = cur.next
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
 
-    num1 = ''.join(temp1)
-    num2 = ''.join(temp2)
+    return dummy.next
 
-    total = int(num1) + int(num2)
-    strTotal = str(total)
-    print(strTotal)
-    ansHead = None
-    for char in strTotal:
-        node = ListNode(int(char))
-        node.next = ansHead
-        ansHead = node
+def iter_vals(node):
+    while node:
+        yield node.val
+        node = node.next
 
-    return ansHead
-
-result = addTwoNumbers(l1, l2)
-print(result)
+res = addTwoNumbers(l1, l2)
+vals = list(iter_vals(res))
+for i, v in enumerate(reversed(vals)):
+    print(v, end=' -> ' if i < len(vals) - 1 else '\n')
