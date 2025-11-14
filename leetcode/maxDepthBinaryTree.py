c8a64416e2fa79ad1904e7d85d4a613627603351
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-11-14 13:23:54 +0000

    max depth dfs

--- leetcode/maxDepthBinaryTree.py
+++ leetcode/maxDepthBinaryTree.py
@@ -14,8 +14,23 @@ root.right = TreeNode(7)
 root.right.right = TreeNode(9)
 root.right.left = TreeNode(6)
 
-def invertTreeDFS(root: TreeNode) -> int:
-    pass
+def maxDepthTreeDFS(root: TreeNode) -> int:
+    if not root:
+        return 0
+    stack = [(root, 1)]
+    maxDepth = 0
+    while stack:
+        cur, curDep = stack.pop()
+        maxDepth = max(maxDepth, curDep)
+        if cur.right:
+            stack.append((cur.right, curDep + 1))
+        if cur.left:
+            stack.append((cur.left, curDep + 1))
+
+    return maxDepth
 
 def invertTreeBFS(root: TreeNode) -> int:
     pass
+
+result = maxDepthTreeDFS(root)
+print(result)
