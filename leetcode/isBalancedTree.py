9df930460ce82d1db5e07a33bbddad9033fc1272
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-11-16 14:21:35 +0000

    isBalanced done

--- leetcode/isBalancedTree.py
+++ leetcode/isBalancedTree.py
@@ -39,7 +39,16 @@ rootTwo.left.left.left = TreeNode(5)
 #
 
 def isBalanced(root: TreeNode) -> bool:
-    pass
+
+    def dfs(root):
+        if not root:
+            return [True, 0]
+
+        left, right = dfs(root.left), dfs(root.right)
+        balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+        return [balanced, 1 + max(left[1], right[1])]
+
+    return dfs(root)[0]
 
 GREEN = "\033[92m"
 RED = "\033[91m"
