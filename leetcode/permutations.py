8433e63fd8412bc01270a521e6c3b2f09c0b4140
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-12-12 13:25:42 +0000

    really need help with backtracking

--- leetcode/permutations.py
+++ leetcode/permutations.py
@@ -7,7 +7,17 @@ tests = [
 ]
 
 def perm(nums: list) -> list[list[int]]:
-    pass
+    if len(nums) == 0:
+            return [[]]
+
+    perms = perm(nums[1:])
+    res = []
+    for p in perms:
+        for i in range(len(p) + 1):
+            p_copy = p.copy()
+            p_copy.insert(i, nums[0])
+            res.append(p_copy)
+    return res 
 
 GREEN = "\033[92m"
 PURPLE = "\033[95m"
