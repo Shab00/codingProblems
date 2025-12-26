a83aa8a40750a26706fdc237013de084e3afb6dc
Shab00 <47157957+Shab00@users.noreply.github.com>
2025-12-26 15:06:30 +0000

    started firt heap question

--- leetcode/kthLargeElementInStream.py
+++ leetcode/kthLargeElementInStream.py
@@ -1,11 +1,27 @@
-class KthLargest:
+import heapq
 
-    def __init__(self, k: int, nums: List[int]):
-        pass 
+class KthLargest:
+    def __init__(self, k: int, nums: list[int]):
+        self.k = k
+        self.heap = list(nums)
+        heapq.heapify(self.heap)
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
 
     def add(self, val: int) -> int:
-        pass
+        heapq.heappush(self.heap, val)
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+
+        return self.heap[0]
     
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
+
+obj = KthLargest(3, [4, 5, 8, 2])
+p1 = obj.add(3)
+p2 = obj.add(5)
+p3 = obj.add(10)
+p4 = obj.add(9)
+p5 = obj.add(8)
