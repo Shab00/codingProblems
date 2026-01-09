9939c5e7b70a95405ac904f96c925a3ea561961e
Shab00 <47157957+Shab00@users.noreply.github.com>
2026-01-09 19:10:22 +0000

    finished task schedular

--- leetcode/taskSchedular.py
+++ leetcode/taskSchedular.py
@@ -3,12 +3,35 @@ import heapq
 inputs = [
         (["A","A","A","B","B","B"], 2, 8),
         (["A","C","A","B","D","B"], 1, 6),
-        (["A","A","A", "B","B","B"], 3, 10)
+        (["A","A","A","B","B","B"], 3, 10)
         ]
 
 def least(task: list[str], n: int) -> int:
-    pass
-
+    if n == 0:
+        return len(task)
+    hmap = {}
+    for letters in task:
+        if letters not in hmap:
+            hmap[letters] = 0
+        hmap[letters] += 1
+    heap = [(-v, k) for k, v in  hmap.items()]
+    heapq.heapify(heap) 
+    t = 0
+    while heap:
+        temp = []
+        ex = 0
+        for _ in range(n + 1):
+            if not heap:
+                break
+            val, key = heapq.heappop(heap)
+            val += 1
+            if val < 0:
+                temp.append((val, key))
+            ex += 1
+        for item in temp:
+            heapq.heappush(heap, item)
+        t += (n + 1) if heap else ex
+    return t
 
 GREEN = "\033[92m"
 RED = "\033[91m"
