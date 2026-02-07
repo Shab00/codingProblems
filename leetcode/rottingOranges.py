e4d579e6b8abea55619539cab3f0397934a1f80c
Shab00 <47157957+Shab00@users.noreply.github.com>
2026-02-07 20:34:48 +0000

    finished rotting oranges

--- leetcode/rottingOranges.py
+++ leetcode/rottingOranges.py
@@ -17,30 +17,38 @@ def ornages(grid: list[list[int]]) -> int:
     m = len(grid)
     n = len(grid[0])
     visted = set()
-    count = 0
+    fresh = 0
+    time = 0
     directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     for row in range(m):
         for col in range(n):
-            if grid[row][col] == 1:
-                for dr, dc in directions:
-                    nr, nc = dr + row, dc + col
-                    if 0 <= nr < m and 0 <= nc < n:
-                        if grid[nr][nc] == 0: 
-            if grid[row][col] == 2 and (row, col) not in visted:
+            if grid[row][col] == 1 and (row, col) not in visted:
+                fresh += 1
+            elif grid[row][col] == 2 and (row, col) not in visted:
                 q.append((row, col))
                 visted.add((row, col))
 
-    while q:
-        row, col = q.popleft()
+    if fresh == 0:
+        return 0
 
-        for dr, dc in directions:
-            nr, nc = dr + row, dc + col
-            if 0 <= nr < m and 0 <= nc < n:
-                if grid[nr][nc] == 1 and (nr, nc) not in visted:
-                    grid[nr][nc] = 2
-                    q.append((nr, nc))
-                    visted.add((nr, nc))
-                    print(grid)
+    while q:
+        infect = 0
+        for _ in range(len(q)):
+            row, col = q.popleft()
+            for dr, dc in directions:
+                nr, nc = dr + row, dc + col
+                if 0 <= nr < m and 0 <= nc < n:
+                    if grid[nr][nc] == 1 and (nr, nc) not in visted:
+                        grid[nr][nc] = 2
+                        q.append((nr, nc))
+                        visted.add((nr, nc))
+                        fresh -= 1 
+                        infect += 1
+        if infect > 0:
+            time += 1
+        if fresh == 0:
+            return time
+    return -1
 
 GREEN = "\033[92m"
 RED = "\033[91m"
