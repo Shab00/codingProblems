78750863d201d906ae641e40a5a45c676c3e3715
Shab00 <47157957+Shab00@users.noreply.github.com>
2026-02-12 21:40:11 +0000

    finished course schedule

--- leetcode/courseSchedule.py
+++ leetcode/courseSchedule.py
@@ -1,12 +1,32 @@
-class Node:
-    def __init__ (course = None, pre = None):
-        self.course = course
-        self.pre = pre 
-
 inputs = [(2, [[1,0]], True), (2, [[1,0],[0,1]], False)]
 
 def cycle(numCourses: int, prerequisites: list[list[int]]) -> bool:
-    pass
+    hMap = {i: [] for i in range(numCourses)}
+    for course, pre in prerequisites:
+        hMap[course].append(pre)
+    visting = set()
+    def dfs(course):
+        if course in visting:
+            return False
+        if hMap[course] == []:
+            return True  
+
+        visting.add(course)
+
+        for pre in hMap[course]:
+            if not dfs(pre):
+                return False
+
+        visting.remove(course)
+
+        hMap[course] = []
+        return True
+
+    for c in range(numCourses):
+        if not dfs(c):
+            return False
+    return True
+
 
 
 GREEN = "\033[92m"
