0e5201ad469c85b3ab77e050fab8f1dd79519390
Shab00 <47157957+Shab00@users.noreply.github.com>
2026-02-25 08:49:30 +0000

    quick test

--- interviewPractice/longestSubString.py
+++ interviewPractice/longestSubString.py
@@ -1,16 +1,13 @@
 def lengthOfLongestSubstring(s: str) -> int:
     lastIndex = {}
-    left = 0
-    best = 0
+    left, best = 0, 0
 
     for right, ch in enumerate(s):
         if ch in lastIndex and lastIndex[ch] >= left:
             left = lastIndex[ch] + 1
-
         lastIndex[ch] = right
         best = max(best, (right - left + 1))
     return best
-
 if __name__ == "__main__":
     print(lengthOfLongestSubstring("abcabcbb"))  # 3 ("abc")
     print(lengthOfLongestSubstring("bbbbb"))     # 1 ("b")
