f54f876bd6e1be284c0d7ee8cd9cc4f8f28d8e86
Bashaar <47157957+Shab00@users.noreply.github.com>
2026-06-14 23:28:13 +0100

    Finished hard newspaper problem

--- algoMonster/binarySearch/newspapers.py
+++ algoMonster/binarySearch/newspapers.py
@@ -2,18 +2,32 @@ inputs = [([7,2,5,10,8], 2, 18),
           ([2,3,5,7], 3, 7)]
 
 def find(arr: list[int], target: int) -> int:
-    l, r = 0, len(arr) - 1
-
-    best = -1
-
+    l = max(arr)
+    r = sum(arr)
+    ans = r
     while l <= r:
-        mid = (r + l) // 2
+        mid = (l + r) // 2
 
-        leftside = sum(arr[:mid]) 
-        rightside = sum(arr[mid:])
-        if leftside > rightside:
-            
+        if fun(arr, target, mid):
+            ans = mid
+            r = mid - 1
+        else:
+            l = mid + 1
 
+    return ans
+        
+def fun(arr, k, limit):
+    workers = 1
+    current_sum = 0
+
+    for time in arr:
+        if current_sum + time <= limit:
+            current_sum += time
+        else:
+            workers += 1
+            current_sum = time
+
+    return workers <= k
 
 GREEN = "\033[92m"
 RED = "\033[91m"
